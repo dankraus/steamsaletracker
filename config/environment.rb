aaaef0f4f745417ebdfbee8d019a9cab01144571
Dan Kraus <dskraus@gmail.com>
2012-06-06 21:33:04 -0500

    Remove mail initilizer file and move to environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,15 @@ load(heroku_env) if File.exists?(heroku_env)
 
 # Initialize the rails application
 SteamSaleTracker::Application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+	:user_name => ENV['SENDGRID_USERNAME'],
+	:password => ENV['SENDGRID_PASSWORD'],
+	:domain => "steamsaletracker.com",
+	:address => "smtp.sendgrid.net",
+	:port => 587,
+	:authentication => :plain,
+	:enable_starttls_auto => true
+}
